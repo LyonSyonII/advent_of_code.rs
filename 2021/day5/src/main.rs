7ff9064b5ca7f0915a4b61465d295bc8c8de38c9
LyonSyonII <slimystudios@protonmail.com>
2022-05-28 11:23:28 +0200

    Added day5

--- 2021/day5/src/main.rs
+++ 2021/day5/src/main.rs
@@ -1,3 +1,32 @@
+use std::{str::FromStr, num::ParseIntError};
+
+#[derive(Debug, Clone, Copy)]
+struct Line {
+    start: (u16, u16),
+    end: (u16, u16),
+}
+
+#[derive(Debug, Clone)]
+struct Diagram {
+    points: Vec<Line>
+}
+
+
+impl FromStr for Line {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (start, end) = s.split_once(" -> ").unwrap();
+        let (x1, y1) = start.split_once(',').unwrap();
+        let (x2, y2) = end.split_once(',').unwrap();
+        Ok(Line { start: (x1.parse()?, y1.parse()?), end: (x2.parse()?, y2.parse()?)})
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    for line in input.lines() {
+        let line: Line = line.parse().unwrap();
+        println!("{:?}", line)
+    }
 }
